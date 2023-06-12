021e2fedf7e7c15518c2d5a13409836d1f05de6b
Oli Ulllah <62113494+OliUllah1@users.noreply.github.com>
2023-06-12 09:24:25 +0600

    payments information save the mongodb server using payments api

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s5nou2l.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,10 +46,11 @@ async function run() {
     const usersCollection= client.db('drawingSchool').collection('users');
     const classesCollection =client.db('drawingSchool').collection('classes');
     const saveClassesCollection =client.db('drawingSchool').collection('saveClasses');
+    const paymentsCollection =client.db('drawingSchool').collection('payments');
 
-    app.post('/jwt' , (req, res) => {
+    app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '6h' })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
 
       res.send({ token })
     })
@@ -108,8 +125,16 @@ async function run() {
       const result=await classesCollection.findOne(query)
       res.send(result)
     })
-    app.get('/classes', async(req,res)=>{
+    app.get('/classes', verifyJWT, async(req,res)=>{
       const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'forbidden access' })
+      }
+
       const query={instructorEmail:email}
       const result = await classesCollection.find(query).toArray()
       res.send(result)
@@ -176,6 +201,12 @@ async function run() {
       const result = await saveClassesCollection.find().toArray()
       res.send(result)
     })
+    app.get('/saveclass/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query = { _id : id}
+      const result= await saveClassesCollection.findOne(query)
+      res.send(result)
+    })
     app.post('/saveclass', async(req,res)=>{
       const saveClass = req.body;
       const query ={_id:saveClass._id,email:saveClass.email}
@@ -197,10 +228,8 @@ async function run() {
 
     // create payment intent
     app.post('/create-payment-intent', async (req, res) => {
-      const {price}  = req.body;
-      console.log(price)
+      const { price } = req.body;
       const amount = parseInt(price * 100);
-      console.log(price,amount)
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: 'usd',
@@ -212,6 +241,28 @@ async function run() {
       })
     })
 
+
+    // payments related api
+    app.post('/payments', async(req,res)=>{
+      const id = req.query.id;
+      const paymentsData =  req.body;
+      const query ={_id : id}
+      const filter = {_id: new ObjectId(id)}
+      const deleteSaveCard = await saveClassesCollection.deleteOne(query)
+      const classInformation= await classesCollection.findOne(filter)
+      const availableSets=classInformation.availableSets
+      const TotalEnrolledStudents=classInformation.TotalEnrolledStudents;
+      const updateDoc = {
+        $set: {
+          TotalEnrolledStudents: TotalEnrolledStudents + 1,
+          availableSets: availableSets -1
+        },
+      };
+      const updateDocuments= classesCollection.updateOne(filter,updateDoc)
+      const result =await paymentsCollection.insertOne(paymentsData);
+      res.send(result)
+
+    })
 
 
 
